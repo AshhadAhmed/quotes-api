21c7d67202674026d34c1ef75e37235f2d835e44
Ahmed <syedahmed268y@gmail.com>
2025-04-02 18:32:41 +0500

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 import Quote from '../models/quote.model.js';
 
-const connectDB = async (MONGODB_URI) => {
-    const defaultQuotes = [
-        { quote: 'The only way to do great work is to love what you do.', author: 'Steve Jobs', category: 'motivation' },
-        { quote: 'Love all, trust a few, do wrong to none.', author: 'William Shakespeare', category: 'love' },
-        { quote: 'Don\'t watch the clock; do what it does. Keep going.', author: 'Sam Levenson', category: 'success' },
-        { quote: 'Believe you can and you\'re halfway there.', author: 'Theodore Roosevelt', category: 'inspiration' },
-        { quote: 'Life is what happens when you\'re busy making other plans.', author: 'John Lennon', category: 'general' },
-    ];
+const defaultQuotes = [
+    { quote: 'The only way to do great work is to love what you do.', author: 'Steve Jobs', category: 'motivation' },
+    { quote: 'Love all, trust a few, do wrong to none.', author: 'William Shakespeare', category: 'love' },
+    { quote: 'Don\'t watch the clock; do what it does. Keep going.', author: 'Sam Levenson', category: 'success' },
+    { quote: 'Believe you can and you\'re halfway there.', author: 'Theodore Roosevelt', category: 'inspiration' },
+    { quote: 'Life is what happens when you\'re busy making other plans.', author: 'John Lennon', category: 'general' },
+];
 
+const connectDB = async (MONGODB_URI) => {
     try {
         await mongoose.connect(MONGODB_URI);
         console.log('Connected to MongoDB.');
 
-        const existingQuote = await Quote.findOne();
+        const quoteCount = await Quote.countDocuments();
 
-        if (!existingQuote) {
+        if (quoteCount === 0) {
             await Quote.insertMany(defaultQuotes);
             console.log('Default quotes added to the database.');
         } else {
