375953119c77f28cffaed1e93e7c2f3b4d81dc17
Ahmed <syedahmed268y@gmail.com>
2025-04-04 10:32:27 +0500

    Added endpoint for sign out

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signIn, signUp } from '../controllers/auth.controller.js';
+import { signIn, signUp, signOut } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/signin', signIn);
 router.post('/signup', signUp);
+router.post('/logout', signOut);
 
 export default router;
