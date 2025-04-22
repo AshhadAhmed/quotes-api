6f4c0c33971c16c5027eceffe5dc96ea151f70f5
Ashhad <syedahmed268y@gmail.com>
2025-04-22 12:50:23 +0500

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,9 @@ import { authenticate } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.post('/signin', signIn);
-router.post('/signup', signUp);
-router.post('/logout', signOut);
+router.post('/sign-in', signIn);
+router.post('/sign-up', signUp);
+router.post('/sign-out', signOut);
 router.delete('/delete', authenticate, deleteAccount);
 
 export default router;
