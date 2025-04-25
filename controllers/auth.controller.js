37002b8661dbfd5d2b30729c4c26aff702df3ec5
AshhadAhmed <syedahmed268y@gmail.com>
2025-04-25 16:18:13 +0500

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,13 +14,13 @@ import HttpError from '../utils/HttpError.js';
 export const signIn = async function (req, res, next) {
     const { email, password } = req.body;
 
-    if (!email || !password) next(new HttpError('Missing email or password', 400));
+    if (!email || !password) return next(new HttpError('Missing email or password', 400));
     const user = await User.findOne({ email });      // check if the user already exists
 
-    if (!user) next(new HttpError('Invalid email address', 404));
+    if (!user) return next(new HttpError('Invalid email address', 404));
     const isValidPassword = await bcrypt.compare(password, user.password);
 
-    if (!isValidPassword) next(new HttpError('Invalid password', 401));
+    if (!isValidPassword) return next(new HttpError('Invalid password', 401));
 
     // payload (data to be transmitted)
     const payload = { id: user._id, role: user.role };
@@ -41,7 +41,7 @@ export const signIn = async function (req, res, next) {
         sameSite: 'Strict',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    return res.json({
+    res.json({
         success: true,
         message: 'Signed in successfully',
         data: { token: newAccessToken },
@@ -51,13 +51,13 @@ export const signIn = async function (req, res, next) {
 export const signUp = async function (req, res, next) {
     const { email, password } = req.body;
 
-    if (!email || !password) next(new HttpError('Missing email or password', 400));
+    if (!email || !password) return next(new HttpError('Missing email or password', 400));
 
     if (password.length < 6)
-        next(new HttpError('Password must be at least 6 characters long', 400));
+        return next(new HttpError('Password must be at least 6 characters long', 400));
 
     const existingUser = await User.findOne({ email });
-    if (existingUser) next(new HttpError('The email address already exists', 409));
+    if (existingUser) return next(new HttpError('The email address already exists', 409));
 
     // hashing the password
     const salt = await bcrypt.genSalt(10);
@@ -83,7 +83,7 @@ export const signUp = async function (req, res, next) {
         sameSite: 'Strict',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    return res.status(201).json({
+    res.status(201).json({
         success: true,
         message: 'Signed up successfully',
         data: { token: accessToken },
@@ -97,22 +97,21 @@ export const signOut = function (req, res) {
         sameSite: 'Strict',
     });
 
-    return res.json({ success: true, message: 'Signed out successfully' });
+    res.json({ success: true, message: 'Signed out successfully' });
 };
 
 export const deleteAccount = async function (req, res, next) {
-    if (!req.user || !req.user.id) next(new HttpError('Unauthorized', 401));
+    if (!req.user || !req.user.id) return next(new HttpError('Unauthorized', 401));
 
     const { id, role } = req.user;
-    if (!isValidObjectId(id)) next(new HttpError('Invalid ID', 400));
+    if (!isValidObjectId(id)) return next(new HttpError('Invalid ID', 400));
 
-    if (role === 'admin') next(new HttpError('Admin account cannot be deleted', 403));
+    if (role === 'admin') return next(new HttpError('Admin account cannot be deleted', 403));
 
     const deletedUser = await User.findByIdAndDelete(id);
-    if (!deletedUser) next(new HttpError('User not found', 404));
+    if (!deletedUser) return next(new HttpError('User not found', 404));
 
     // delete related quotes by user
     await Quote.deleteMany({ createdBy: id });
-
-    return res.json({ success: true, message: 'Account deleted successfully' });
+    res.json({ success: true, message: 'Account deleted successfully' });
 };
