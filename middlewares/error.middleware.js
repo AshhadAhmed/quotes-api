e1f4947bff8d0c934b3e709a81d0159052ef7a96
AshhadAhmed <syedahmed268y@gmail.com>
2025-04-25 16:09:13 +0500

    Update error.middleware.js

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,3 +1,10 @@
 export const notFound = function (req, res, next) {
     res.status(404).json({ success: false, message: 'Route not found' });
 };
+
+export const errorHandler = function (err, req, res, next) {
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || 'Internal server error',
+    });
+};
